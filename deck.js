bd1f2656bdb2143b1fefb977ad39f82dde309503
afrianjunior <afrian.junior26@gmail.com>
2019-08-24 04:30:40 +0700

    init

---
+++ deck.js
@@ -0,0 +1,20 @@
+export { nightOwlFull as theme } from 'code-surfer/themes'
+import { slides as introduction } from './introduction.mdx'
+import { slides as Basic } from './Basic.mdx'
+import { slides as FindYourOwnPath } from './FindYourOwnPath.mdx'
+import { slides as JackVsMaster } from './JackVsMaster.mdx'
+import { slides as CleanCode } from './CleanCode.mdx'
+import { slides as IncreaseSkill } from './IncreaseSkill.mdx'
+import { slides as UserOriented } from './UserOriented.mdx'
+import { slides as Summary } from './Summary.mdx'
+
+export const slides = [
+    ...introduction,
+    ...Basic,
+    ...FindYourOwnPath,
+    ...JackVsMaster,
+    ...CleanCode,
+    ...IncreaseSkill,
+    ...UserOriented,
+    ...Summary
+]
